23374a3c13cbce2cba59aceeeb43ba78c36ec442
Rai <rzshntrrs09@gmail.com>
2026-09-11 10:25:59 +0800

    may inayos lang css

--- spaces-bulsu/src/Components/LoginNav/LoginNav.jsx
+++ spaces-bulsu/src/Components/LoginNav/LoginNav.jsx
@@ -21,7 +21,7 @@ export default function LoginNav({ activePage, onChangePage, onSignIn, onLogout 
         />
 
         <div>
-          <p className="brand-name">Spaces CICT</p>
+          <p className="brand-name">SpaceS CICT</p>
         </div>
       </div>
       
