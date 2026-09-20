3a5589f036b5ebcfc0a29fc9caafc457c1175af2
Dan Ivan Cajucom <danivan.cajucom@gmail.com>
2026-09-20 13:23:45 +0800

    Use CJS wrapper with dynamic import for Vercel ESM

--- spaces-bulsu/api/index.js
+++ spaces-bulsu/api/index.js
@@ -1,3 +1,9 @@
-import app from '../src/backend/index.js';
+﻿let appPromise;
 
-export default app;
+module.exports = async (req, res) => {
+  if (!appPromise) {
+    appPromise = import("../src/backend/index.js").then((m) => m.default);
+  }
+  const app = await appPromise;
+  return app(req, res);
+};
